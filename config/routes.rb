31da123e74aab686a7ad34eff0fe343b4b24b114
alifdevz <alifm.devz@gmail.com>
2023-01-04 17:38:52 +0800

    mount Action cable server

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  # Action cable server
+  mount ActionCable.server => '/cable'
   resources :messages
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
