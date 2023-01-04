c082050c1332c1ba9d5bed7b8440840537efe534
alifdevz <alifm.devz@gmail.com>
2023-01-04 19:05:41 +0800

    Fix id and body have no values issue

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,11 +1,13 @@
 class Message < ApplicationRecord
     after_create_commit { broadcast_message }
-
+  
     private
+  
     def broadcast_message
-        ActionCable.server.broadcast("MessagesChannel", {
-                                        id:,
-                                        body:
-                                    })
+      ActionCable.server.broadcast('MessagesChannel',{ 
+                                    id: 1, 
+                                    body: "Hi!" }
+                                    )
     end
-end
+  end
+  
